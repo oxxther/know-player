414564091c9d189e6850a3c76a796f9f55e53258
user <508131409@qq.com>
2021-09-18 16:24:31 +0800

    2

--- OXXKnowPlayer.podspec
+++ OXXKnowPlayer.podspec
@@ -2,11 +2,12 @@
 Pod::Spec.new do |spec|
   spec.name                 = "OXXKnowPlayer"
   spec.version              = "0.0.1"
-  spec.summary              = "A short description of OXXKnowPlayer."
+  spec.summary              = "AVFoundation Learn"
   spec.homepage             = "https://github.com/oxxther/know-player"
   spec.license              = "MIT"
   spec.author               = { "oxxther" => "508131409@qq.com" }
   spec.platform             = :ios, "9.0"
   spec.source               = { :git => "https://github.com/oxxther/know-player.git", :tag => "#{spec.version}" }
   spec.source_files         = "OXXKnowPlayer/*.swift"
+  spec.swift_versions       = "5.0"
 end
--- OXXKnowPlayer/OXXPlayerViewController.swift
+++ OXXKnowPlayer/OXXPlayerViewController.swift
@@ -13,14 +13,14 @@ class OXXPlayerViewController: UIViewController {
     
     //MARK: properties
     fileprivate var player : AVPlayer = AVPlayer()
-    fileprivate var playerView : OXXPlayerView = OXXPlayerView()
+//    fileprivate var playerView : OXXPlayerView = OXXPlayerView()
     
     //MARK: life cycle
-    override func loadView() {
-        super.loadView()
-        playerView.frame = view.frame
-        view = playerView
-    }
+//    override func loadView() {
+//        super.loadView()
+//        playerView.frame = view.frame
+//        view = playerView
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,18 +32,18 @@ class OXXPlayerViewController: UIViewController {
 
 }
 
-//MARK: public
-extension OXXPlayerViewController {
-    func play(_ url: String) {
-        guard let _url = URL(string: url) else {
-            return
-        }
-        let item = AVPlayerItem(url: _url)
-        player.replaceCurrentItem(with: item)
-        let playerLayer = AVPlayerLayer(player: player)
-        playerLayer.frame = playerView.bounds
-        playerView.layer.insertSublayer(playerLayer, at: 0)
-        player.play()
-    }
-}
+////MARK: public
+//extension OXXPlayerViewController {
+//    func play(_ url: String) {
+//        guard let _url = URL(string: url) else {
+//            return
+//        }
+//        let item = AVPlayerItem(url: _url)
+//        player.replaceCurrentItem(with: item)
+//        let playerLayer = AVPlayerLayer(player: player)
+//        playerLayer.frame = playerView.bounds
+//        playerView.layer.insertSublayer(playerLayer, at: 0)
+//        player.play()
+//    }
+//}
 
